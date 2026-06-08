53225a97ac145891a0ec9824f1903063267ef5e7
sespure <andrijcernicka92@gmail.com>
2026-06-08 22:56:47 +0300

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by sespure on 08.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
